4841c9956d497588ecd46ddd30283cb57a93d865
1 <1>
2024-12-06 11:33:52 +0800

    完整流程

--- web/electron/test_1/renderer.js
+++ web/electron/test_1/renderer.js
@@ -1,5 +1,7 @@
-// const information = document.getElementById('info')
-// information.innerText = `本应用正在使用 Chrome (v${versions.chrome()}), Node.js (v${versions.node()}), 和 Electron (v${versions.electron()})`
+
+
+const information = document.getElementById('info')
+information.innerText = `Chrome (v${versions.chrome()}), Node.js (v${versions.node()}), 和 Electron (v${versions.electron()})`
 
 
 const func = async () =>
